1310340c1f472b502be41a24496305e26fed1dfe
Erik Aigner <aigner.erik@gmail.com>
2018-01-06 03:47:35 +0100

    Trits to tryte string

--- trinary/trits.go
+++ trinary/trits.go
@@ -1,5 +1,9 @@
 package trinary
 
+const (
+	tryteAlphabet = "9ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
+
 type Trits struct {
 	a []int8
 }
@@ -24,4 +28,28 @@ func (t Trits) Len() int {
 
 func (t Trits) At(i int) int8 {
 	return t.a[i]
+}
+
+func (t Trits) ToTrytes(v *Trytes) bool {
+	if t.Len()%3 != 0 {
+		return false
+	}
+	n := t.Len() / 3
+	v.buf = make([]byte, n)
+	for i := 0; i < n; i++ {
+		j := t.At(i*3) + t.At(i*3+1)*3 + t.At(i*3+2)*9
+		if j < 0 {
+			j += int8(len(tryteAlphabet))
+		}
+		v.buf[i] = tryteAlphabet[j]
+	}
+	return true
+}
+
+type Trytes struct {
+	buf []byte
+}
+
+func (t Trytes) String() string {
+	return string(t.buf)
 }
--- trinary/trits_test.go
+++ trinary/trits_test.go
@@ -32,3 +32,53 @@ func TestTritsFromInt8(t *testing.T) {
 		}
 	}
 }
+
+func TestTritsAt(t *testing.T) {
+	var trits Trits
+	a := []int8{-1, 0, 1, 0, -1}
+	if !TritsFromInt8(a, &trits) {
+		t.Fail()
+	}
+	for i, v := range a {
+		if trits.At(i) != v {
+			t.Fail()
+		}
+	}
+}
+
+func TestTritsToTrytes(t *testing.T) {
+	type test struct {
+		in     []int8
+		result string
+	}
+
+	var table []test
+
+	for i := -1; i < 2; i++ {
+		for j := -1; j < 2; j++ {
+			for k := -1; k < 2; k++ {
+				v := []int8{int8(k), int8(j), int8(i)}
+				c := tryteAlphabet[(len(table)+14)%len(tryteAlphabet)]
+				table = append(table, test{v, string(c)})
+			}
+		}
+	}
+
+	if len(table) != len(tryteAlphabet) {
+		t.Fatal("invalid table length")
+	}
+
+	for _, v := range table {
+		var trits Trits
+		if !TritsFromInt8(v.in, &trits) {
+			t.Fail()
+		}
+		var trytes Trytes
+		if !trits.ToTrytes(&trytes) {
+			t.Fail()
+		}
+		if s := trytes.String(); s != v.result {
+			t.Logf("is=%s, want=%s", s, v.result)
+		}
+	}
+}

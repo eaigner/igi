b72efdb79920155283a03f7330d8250afa2445ce
Erik Aigner <aigner.erik@gmail.com>
2018-01-22 22:06:19 +0100

    Store interface

--- storage/store.go
+++ storage/store.go
@@ -1,6 +1,42 @@
 package storage
 
+const (
+	TransactionBucket Bucket = iota + 1
+	TransactionMetadataBucket
+	MilestoneBucket
+	StateDiffBucket
+	AddressBucket
+	ApproveeBucket
+	BundleBucket
+	TagBucket
+)
+
+type Bucket int
+
+type Entry struct {
+	bucket Bucket
+	key    []byte
+	bytes  []byte
+}
+
 type Store interface {
-	Set(bytes []byte, key, domain string) bool
-	Get(key, domain string) ([]byte, bool)
+	// WriteBatch writes a batch of entries to the DB.
+	WriteBatch(batch []Entry) error
+
+	// ReadBatch reads a batch of entries from the DB. Upon success, the bytes value for each entry should be set.
+	ReadBatch(batch []Entry) error
+}
+
+// Write is a convenience method to perform a single entry write.
+func Write(s Store, key, bytes []byte, bucket Bucket) error {
+	return s.WriteBatch([]Entry{{bucket: bucket, key: key, bytes: bytes}})
+}
+
+// Read is a convenience method to perform a single entry read.
+func Read(s Store, key []byte, bucket Bucket) ([]byte, error) {
+	entry := Entry{bucket: bucket, key: key, bytes: nil}
+	if err := s.ReadBatch([]Entry{entry}); err != nil {
+		return nil, err
+	}
+	return entry.bytes, nil
 }

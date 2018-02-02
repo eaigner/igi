6a013f79d271939da3fc3c244be170ab92fa3ef1
Erik Aigner <aigner.erik@gmail.com>
2018-02-02 15:50:07 +0100

    Test Exists before Write

--- storage/bolt_test.go
+++ storage/bolt_test.go
@@ -7,6 +7,9 @@ import (
 
 func TestExists(t *testing.T) {
 	dbPath := "test_exists.db"
+
+	os.Remove(dbPath)
+
 	s, err := NewBoltStore(dbPath)
 	if err != nil {
 		t.Fatal(err)
@@ -17,11 +20,20 @@ func TestExists(t *testing.T) {
 	k := []byte("testKey")
 	v := []byte("testValue")
 
+	exists, err := Exists(s, k, TransactionBucket)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatal("should not exist yet")
+	}
+
 	if err := Write(s, k, v, TransactionBucket); err != nil {
 		t.Fatal(err)
 	}
 
-	exists, err := Exists(s, k, TransactionBucket)
+	exists, err = Exists(s, k, TransactionBucket)
 
 	if err != nil {
 		t.Fatal(err)
